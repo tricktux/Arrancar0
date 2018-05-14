d43398d7974ad25c01f9e4d218036d3acad552dd
Reinaldo Molina <rmolin88@gmail.com>
2018-05-14 06:18:14 -0400

    Following s2client-api example exercises

--- src/Arrancar0.cpp
+++ src/Arrancar0.cpp
@@ -1,3 +1,32 @@
-int main(int /*argc*/, char* /*argv*/[]) {
+#include <sc2api/sc2_api.h>
+
+#include <iostream>
+
+class Bot : public sc2::Agent {
+public:
+    virtual void OnGameStart() final {
+        std::cout << "Hello, World!" << std::endl;
+    }
+
+    virtual void OnStep() final {
+        std::cout << Observation()->GetGameLoop() << std::endl;
+    }
+};
+
+int main(int argc, char* argv[]) {
+	sc2::Coordinator coordinator;
+    coordinator.LoadSettings(argc, argv);
+
+    Bot bot;
+    coordinator.SetParticipants({
+		sc2::CreateParticipant(sc2::Race::Terran, &bot),
+        sc2::CreateComputer(sc2::Race::Zerg)
+    });
+
+    coordinator.LaunchStarcraft();
+    coordinator.StartGame(sc2::kMapBelShirVestigeLE);
+
+    while (coordinator.Update()) {}
+
     return 0;
 }
