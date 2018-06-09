ff693ff07d69920d3358326d11810b7335823a02
Reinaldo Molina <rmolin88@gmail.com>
2018-06-08 22:20:31 -0400

    Improving file detection

--- src/config.cpp
+++ src/config.cpp
@@ -10,9 +10,12 @@
 #include <string_view>
 #include <vector>
 #include <map>
+#include <fstream>
 
 #include <rapidjson/document.h>
+#include <rapidjson/error/en.h>
 #include <glog/logging.h>
+
 #include "../inc/config.hpp"
 
 void config::parse_arguments(int num_options, char* options[])
@@ -31,7 +34,6 @@ void config::parse_arguments(int num_options, char* options[])
 	{
 		if ((!opt.empty()) && (opt.size() > 3) && (opt.compare(config_cmd_option) == 0))
 		{
-
 			LOG(INFO) << "Found config option: " << opt << "\n";
 			config_file_location = opt.substr(3);
 			found = true;
@@ -40,18 +42,21 @@ void config::parse_arguments(int num_options, char* options[])
 	}
 
 	if (found == false)
-		LOG(WARNING) << "configuration option (-c) not provided, using default\n";
+		LOG(WARNING) << "Configuration option (-c) not provided, using default\n";
 }
 
 int config::parse_config_file(void)
 {
+
+
 	config_file.Parse(config_file_location.c_str());
 
 	if (config_file.HasParseError())
 	{
 		LOG(ERROR)	<< "Error ("
 					<< static_cast<unsigned>(config_file.GetErrorOffset())
-					<< "): Need to figure out how to get error"
+					<< "): " 
+					<< rapidjson::GetParseError_En(config_file.GetParseError())
 					<< '\n';
 		return -1;
 	}
