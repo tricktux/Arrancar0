04dab5c758b4d412bc03faeb18f051527c97f36a
Reinaldo Molina <rmolin88@gmail.com>
2018-08-05 10:46:58 -0400

    Fixed to reflect latest config file

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -25,9 +25,9 @@ TEST(Config, DefaultConfigFile) {
 
 	EXPECT_EQ(cfg.get_value("replay", "name", s), 1);
 	LOG(INFO) << "s = '" << s << "'";
-	EXPECT_EQ(cfg.get_value("coordinator", "-m", s), 1);
+	EXPECT_EQ(cfg.get_value("coordinator", "map", s), 1);
 	LOG(INFO) << "s = '" << s << "'";
-	EXPECT_EQ(cfg.get_value("coordinator", "-e", s), 1);
+	EXPECT_EQ(cfg.get_value("coordinator", "your_race", s), 1);
 	LOG(INFO) << "s = '" << s << "'";
 }
 
