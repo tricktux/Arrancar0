faf7d4b0e160d69e6bbad4d064e60d7e0912a222
Reinaldo Molina <rmolin88@gmail.com>
2018-09-22 06:18:30 -0400

    Added generation of documentation, plus better descriptions and usage of processors

--- scripts/sc2api_install.sh
+++ scripts/sc2api_install.sh
@@ -4,28 +4,34 @@
 # Email:          rmolin88 at gmail dot com
 # Revision:	    0.0.0
 # Created:        Mon May 14 2018 05:56
-# Last Modified:  Mon May 14 2018 05:56
+# Last Modified: Sat Sep 22 2018 06:14
 
 # NOTE: Modify here to the path to the built root folder of:
 # [s2client-api](https://github.com/Blizzard/s2client-api)
 cd ~/Documents/ML_SC2/s2client-api
+echo "Downloding submodules"
 git submodule update --recursive --init
+
+echo "Building. This may take a minute..."
 mkdir build
 cd build
 cmake .. || exit 1
-make -j8 || exit 2
+make -j$(nproc) || exit 2
 cd ..
 
-# Install SC2 API headers
+echo "Installing SC2 API headers"
 cp -R include/sc2api ~/.local/include
 cp -R include/sc2utils ~/.local/include
 cp -R include/sc2lib ~/.local/include
 cp -R include/sc2renderer ~/.local/include
 cp -R build/generated/s2clientprotocol ~/.local/include
 
-# Install protobuf headers
+echo "Installing protobuf headers"
 cp -R contrib/protobuf/src/google ~/.local/include/sc2api
 
-# Install SC2 API libraries
+echo "Installing SC2 API libraries"
 mkdir -p ~/.local/lib/sc2api
 cp build/bin/*.a ~/.local/lib/sc2api
+
+echo "Generating doxygen docs"
+doxygen Doxyfile
